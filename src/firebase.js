c6f30a3f25f3cbf7e721cb72faccb25bcce10be4
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-12-05 00:12:52 -0600

    add HomeScreen & build Nav

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCikJAe9FlCd4b4oG3KRgTtgJVKekAnS0c",
+  authDomain: "netflix-clone-7769d.firebaseapp.com",
+  projectId: "netflix-clone-7769d",
+  storageBucket: "netflix-clone-7769d.appspot.com",
+  messagingSenderId: "234012744683",
+  appId: "1:234012744683:web:ab6f4c7a6864b559f597ae",
+};
