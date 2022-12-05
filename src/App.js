84d111aaf2e785efabee3a5eb56e46d5845a3d2d
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-12-05 13:43:44 -0600

    building Banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./HomeScreen/HomeScreen";
 
 function App() {
   return (
---
+++ src/Banner/Banner.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  function truncate(string, n) {
+    return string?.length > n ? string.substring(0, n - 1) + "..." : string;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAACuCAMAAAClZfCTAAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BMQEAAADCoPVPbQhfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABeA8XKAAFZcBBuAAAAAElFTkSuQmCC")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">Movie Name</h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">
+          {truncate(`This is a test description`, 150)}
+        </h1>
+      </div>
+
+      <div className="banner--fadeBottom" />
+    </header>
+  );
+}
+
+export default Banner;
