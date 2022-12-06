42fa9f33485bda90cf67cc3257c8ed1131cc0413
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-12-05 19:09:38 -0600

    build LoginScreen Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen/HomeScreen";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
       <Router>
-        <Routes>
-          <Route exact path="/" element={<HomeScreen />} />
-        </Routes>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
       </Router>
     </div>
   );
