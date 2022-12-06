b1a4b63385a161ec5fc1153ff4d4a208ac2a79db
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-12-05 23:37:29 -0600

    implement login & logout functionality

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@redux/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import useReducer from "../features/userSlice.js";
 
 export default configureStore({
