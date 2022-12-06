646b3be3b504389f4156951426bff5eee22170be
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-12-05 22:35:38 -0600

    Firebase Authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCikJAe9FlCd4b4oG3KRgTtgJVKekAnS0c",
@@ -11,7 +13,6 @@ const firebaseConfig = {
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
-const auth = firebase.auth;
+const auth = firebase.auth();
 
-export { auth };
-export default db;
+export { auth, db };
