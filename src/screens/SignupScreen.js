878dccacc162db09419e4783211f48344b645604
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-12-05 20:10:13 -0600

    build SignupScreen Component

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import "./SignupScreen.css";
 
 function SignupScreen() {
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      
+    <div className="signupScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
+
+        <h4>
+          <span className="signupScreen_gray">New to Netflix? </span>
+          <span className="signupScreen_link" onClick={register}>
+            Sign Up now.
+          </span>
+        </h4>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SignupScreen
+export default SignupScreen;
